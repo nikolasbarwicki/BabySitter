21953b1ced44f78985ec5434cb015ecc29df995f
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-08-01 20:47:51 +0200

    Add advanced quering for sitters route, setup geocoding

--- models/Sitter.js
+++ models/Sitter.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
 
 const SitterSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
-  city: {
+  address: {
     type: String,
     required: true,
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    street: String,
+    city: String,
+    state: String,
+    country: String,
   },
   dateOfBirth: {
     type: Date,
@@ -65,6 +81,25 @@ const SitterSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Geocode & create location field
+SitterSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model('sitter', SitterSchema);
--- routes/api/sitters.js
+++ routes/api/sitters.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
+const geocoder = require('../../utils/geocoder');
 
 const Sitter = require('../../models/Sitter');
 
@@ -36,7 +37,7 @@ router.post(
   [
     auth,
     [
-      check('city', 'City is required').not().isEmpty(),
+      check('address', 'Address is required').not().isEmpty(),
       check('dateOfBirth', 'Date of birth is required').not().isEmpty(),
       check('description', 'Description is required').not().isEmpty(),
       check('experience', 'Valid experience is required').isIn([
@@ -57,7 +58,7 @@ router.post(
     }
 
     const {
-      city,
+      address,
       dateOfBirth,
       description,
       experience,
@@ -83,7 +84,7 @@ router.post(
     // Build sitter object
     const sitterFields = {};
     sitterFields.user = req.user.id;
-    if (city) sitterFields.city = city;
+    if (address) sitterFields.address = address;
     if (dateOfBirth) sitterFields.dateOfBirth = dateOfBirth;
     if (description) sitterFields.description = description;
     if (experience) sitterFields.experience = experience;
@@ -146,11 +147,53 @@ router.post(
 // @access  Public
 router.get('/', async (req, res) => {
   try {
-    const sitters = await Sitter.find().populate('user', ['name', 'email']);
-    res.json(sitters);
+    let query;
+
+    // Create query string
+    let queryStr = JSON.stringify(req.query);
+
+    // Create operators ($gt, $gte, etc)
+    queryStr = queryStr.replace(
+      /\b(gt|gte|lt|lte|in)\b/g,
+      (match) => `$${match}`,
+    );
+
+    query = Sitter.find(JSON.parse(queryStr));
+
+    const sitters = await query;
+
+    console.log(queryStr);
+
+    res.status(200).json({
+      success: true,
+      count: sitters.length,
+      data: sitters,
+    });
   } catch (err) {
     console.log(err);
     console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/sitters/user/:user_id
+// @desc    Get sitter profile by user id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const sitter = await Sitter.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'email']);
+
+    if (!sitter)
+      return res.status(400).json({ msg: 'Sitter profile not found' });
+
+    res.json(sitter);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Sitter profile not found' });
+    }
     res.status(500).send('Server Error');
   }
 });
